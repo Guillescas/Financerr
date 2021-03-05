ac0730d43994aa3cc25726aec3adf8f07d7c35cd
Guilherme Illescas <gui.illescas@gmail.com>
2021-03-05 18:45:39 -0300

    Add conditional to show box by type

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,12 @@
 import React, { ReactElement } from 'react';
+import { useTheme } from 'next-themes';
 
-import { FiHome, FiLogOut, FiSettings } from 'react-icons/fi';
+import { FiHome, FiLogOut, FiMoon, FiSettings, FiSun } from 'react-icons/fi';
 import styles from '../styles/components/Sidebar.module.css';
 
 export function Sidebar(): ReactElement {
+  const { theme, setTheme } = useTheme();
+
   return (
     <div className={styles.container}>
       <div className={styles.profile}>
@@ -17,19 +20,39 @@ export function Sidebar(): ReactElement {
 
       <div className={styles.options}>
         <button type="button" className={styles.item}>
-          <FiHome size={32} />
+          <FiHome size={28} />
           <p>Home</p>
         </button>
 
         <button type="button" className={styles.item}>
-          <FiSettings size={32} />
+          <FiSettings size={28} />
           <p>Configurações</p>
         </button>
+
+        {theme === 'light' ? (
+          <button
+            type="button"
+            className={styles.item}
+            onClick={() => setTheme('dark')}
+          >
+            <FiMoon size={28} />
+            <p>Dark mode</p>
+          </button>
+        ) : (
+          <button
+            type="button"
+            className={styles.item}
+            onClick={() => setTheme('light')}
+          >
+            <FiSun size={28} />
+            <p>Light mode</p>
+          </button>
+        )}
       </div>
 
       <div className={styles.logout}>
         <button type="button" className={styles.item}>
-          <FiLogOut size={32} />
+          <FiLogOut size={28} />
           <p>Sair</p>
         </button>
       </div>
