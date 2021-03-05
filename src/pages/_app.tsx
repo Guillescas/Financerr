7a4cc2164fb111d1b618a1febf35c8fdc6b5a3d9
Guilherme Illescas <gui.illescas@gmail.com>
2021-03-05 18:44:31 -0300

    Insert theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import React, { ReactElement } from 'react';
+import { ThemeProvider } from 'next-themes';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: any): ReactElement {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
