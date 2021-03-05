0be5dfbca90e518863de31a095881a011653f5e3
Guilherme Illescas <gui.illescas@gmail.com>
2021-03-04 23:42:27 -0300

    Adapt file to typescript and ESLint rules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import React, { ReactElement } from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: any): ReactElement {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
